a9828842b592022b1c1e3b28e62c8f3b3345cb32
Max Espinosa <maxespinosaschool@gmail.com>
2023-07-22 17:49:58 -0400

    home page done, discover page started

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,29 +1,21 @@
+import NavBar from "./NavBar";
 export default function Home2() {
   return (
     <>
     <div className="overflow-y-hidden h-[832px] relative">
       <div className="w-[1393px] h-[936px] absolute bg-pink-100" />
-
-          <nav className="w-full h-[59px] left-0 top-0 absolute bg-[#8ae79a]">
-            <a href="/" className=" h-14 left-[926px] top-[18px] absolute text-black text-2xl font-light tracking-[4.80px] hover:underline">discover</a>
-
-               <a href="/" className=" h-14 left-[1086px] top-[18px] absolute text-black text-2xl font-light tracking-[4.80px] hover:underline">about</a>
-               
-                <a href="/" className=" h-14 left-[1206px] top-[18px] absolute text-black text-2xl font-light tracking-[4.80px] hover:underline">signup</a>
-
-      <a href="/"className="pr-[34px] pb-[33px] left-[1330px] top-[-9px] absolute justify-start items-center">
-            <img className="w-[75px] h-[75px] top-[592px]" src="/images/favicon.png" />
-          </a>
-
-      </nav>
+     <NavBar/>
 
       <div className="w-[720px] h-[196px] left-[506px] top-[150px] absolute text-black text-9xl font-normal tracking-[12.80px]">WHISKER</div>
       <div className="w-[700px] h-[70px] left-[498px] top-[175px] absolute text-pink-300 text-9xl font-light tracking-[25.60px]">__________</div>
       <div className="h-[163px] left-[719px] top-[311px] absolute text-black text-9xl font-normal tracking-[12px]">WATCH</div>
 
-      <button className="w-[251px] h-[84px] left-[1084px] top-[530px] absolute bg-pink-100 rounded-[50px] border-2 border-black">
-        <div className="w-[214px] h-[74px] text-stone-950 text-[15px] font-light tracking-[8px] flex items-center justify-center pl-9 hover:underline ">START NOW</div>
-      </button>
+      <a
+       href="/discover"
+       className="block w-[251px] h-[84px] left-[1084px] top-[530px] absolute bg-pink-100 rounded-[50px] border-2 border-black hover:bg-pink-200" >
+       <div className="w-[214px] h-[74px]  text-stone-950 text-[17px] font-light tracking-[8px] flex items-center justify-center pl-9 pt-2">START NOW</div>
+      </a>
+
    
       <img className="w-[467px] h-[350px] left-[152px] top-[1017.63px] absolute origin-top-left rotate-[-50.55deg] rounded-[100px]" src="/images/catpink.png" />
       <img className="w-[601.09px] h-[386.46px] left-[-112.56px] top-[531.39px] absolute origin-top-left rotate-[-7deg] rounded-tl-[500px] rounded-tr-[100px] rounded-bl-[100px] rounded-br-[100px]" src="/images/catpink.png" />
---
+++ app/components/NavBar.jsx
@@ -0,0 +1,17 @@
+export default function NavBar(){
+  return(
+
+<nav className="w-full h-[59px] left-0 top-0 absolute bg-[#8ae79a]">
+<a href="/" className=" h-14 left-[966px] top-[18px] absolute text-black text-2xl font-light tracking-[4.80px] hover:underline">home</a>
+
+   <a href="/" className=" h-14 left-[1086px] top-[18px] absolute text-black text-2xl font-light tracking-[4.80px] hover:underline">about</a>
+   
+    <a href="/" className=" h-14 left-[1206px] top-[18px] absolute text-black text-2xl font-light tracking-[4.80px] hover:underline">signup</a>
+
+<a href="/"className="pr-[34px] pb-[33px] left-[1330px] top-[-9px] absolute justify-start items-center">
+<img className="w-[75px] h-[75px] top-[592px]" src="/images/favicon.png" />
+</a>
+
+</nav>
+  )
+}
