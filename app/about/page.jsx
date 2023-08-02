eaee8d0fad808eb6f2beaf28e9cdc6c779952149
Max Espinosa <maxespinosaschool@gmail.com>
2023-08-02 14:24:22 -0400

    about page tweaks

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -8,7 +8,7 @@ export default function AboutPage() {
   <div className="w-[85px] h-[78px] left-[45px] top-[179px] absolute bg-green-300 rounded-full" />
   <img className="w-[800px] h-[832px] left-[650px] top-0 absolute" src="/images/cat-pink-ai.jpg" />
   <div className="w-[66px] h-[846px] left-[617px] top-0 absolute bg-fuchsia-200" />
-  <div className="w-[489px] h-[222px] left-[88px] top-[375px] absolute text-slate-700 text-2xl font-normal">Welcome to the ultimate platform to Discover and track cute cats and lovable stray dogs! We are a passionate community of animal lovers, dedicated to creating a safe and loving environment for our furry friends. </div>
+  <div className="w-[489px] h-[222px] left-[88px] top-[325px] absolute text-slate-700 text-xl font-normal">Welcome to the ultimate platform to Discover and track cute cats and lovable stray dogs! We are a passionate community of animal lovers, dedicated to creating a safe and loving environment for our furry friends. </div>
 
 
   <div className="w-[195px] h-[81px] left-[87px] top-[206px] absolute text-zinc-800 text-[64px] font-bold">hi!</div>
@@ -19,7 +19,7 @@ export default function AboutPage() {
   <img className="w-[634px] h-[832px] left-0 top-0 absolute" src="/images/aicatgreen.jpg" />
   <div className="w-[183px] h-[42px] left-[723px] top-[311px] absolute text-black text-xl font-normal">WHISKER WATCH<br/></div>
   <div className="w-[238px] h-[45px] left-[723px] top-[339px] absolute text-black text-4xl font-bold">our mission...</div>
-  <div className="w-[482px] h-[132px] left-[723px] top-[397px] absolute text-black text-base font-normal leading-relaxed">At our core, we believe in creating a world where every pet receives the love and care it deserves. Our mission is to provide a comprehensive and user-friendly platform that connects animal lovers, fosters responsible pet ownership, and ensures the safety and well-being of every pet.</div>
+  <div className="w-[482px] h-[132px] left-[723px] top-[397px] absolute text-black  font-normal text-xl leading-relaxed">At our core, we believe in creating a world where every pet receives the love and care it deserves. Our mission is to provide a comprehensive and user-friendly platform that connects animal lovers, fosters responsible pet ownership, and ensures the safety and well-being of every pet.</div>
   <div className="w-[75px] h-[72px] left-[685px] top-[213px] absolute bg-pink-300 rounded-full" />
   <div className="w-[109px] h-[104px] left-[810px] top-[655px] absolute bg-pink-200 rounded-full" />
   <div className="w-[39px] h-[39px] left-[1148px] top-[575px] absolute bg-pink-400 rounded-full" />
